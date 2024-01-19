bee4cbd85e732765bd84a492fa3bf318e16769ce
tejraja <96605053+tejraja@users.noreply.github.com>
2024-01-20 00:46:27 +0530

    Update MainActivity3.java

--- MediaPlayer-Android-Application-main/MediaPlayer/app/src/main/java/com/example/musicplayer/MainActivity3.java
+++ MediaPlayer-Android-Application-main/MediaPlayer/app/src/main/java/com/example/musicplayer/MainActivity3.java
@@ -1,14 +1,15 @@
-package com.example.musicplayer;//About As
-
+// Import necessary packages for the About Us activity
+package com.example.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+// Define the MainActivity3 class extending AppCompatActivity
 public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the content view to the layout defined in activity_main3.xml
         setContentView(R.layout.activity_main3);
     }
 }
